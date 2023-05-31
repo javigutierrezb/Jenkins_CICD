20be040113439ba79178a37a8cc7fcf7cbd8c047
Javier Gutiérrez <72932950+javigutierrezb@users.noreply.github.com>
2023-05-31 09:48:08 +0200

    Add files via upload
    Añadiendo proyecto de maven

---
+++ src/main/java/com/example/jgutierrez/pruebasmaven/PruebasmavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.jgutierrez.pruebasmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasmavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jgutierrez/pruebasmaven/PruebasmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jgutierrez.pruebasmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasmavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
